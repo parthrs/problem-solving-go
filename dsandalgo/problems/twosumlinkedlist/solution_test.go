5f349b5f3e64a2363c5a33c4ab5b0dc8f55b271c
Parth Shah <parth@Parths-Macintosh.local>
2021-09-30 22:21:55 -0700

    Simple addition based logic

--- dsandalgo/problems/twosumlinkedlist/solution_test.go
+++ dsandalgo/problems/twosumlinkedlist/solution_test.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"testing"
-
 	sll "github.com/parthrs/LetsGo/dsandalgo/pkg/singlylinkedlist"
+	"testing"
 )
 
 func TestAddTwoNumbers(t *testing.T) {
@@ -16,7 +15,7 @@ func TestAddTwoNumbers(t *testing.T) {
 	l2.Next.Next = sll.NewSinglyNode(4)
 
 	n := AddTwoNumbers(l1, l2)
-	if n.Value != 7 && n.Next.Value != 0 && n.Next.Next.Value != 8 {
+	if n.Value != 7 || n.Next.Value != 0 || n.Next.Next.Value != 8 {
 		t.Errorf("%d%d%d\n != 708", n.Value, n.Next.Value, n.Next.Next.Value)
 	}
 
@@ -35,4 +34,19 @@ func TestAddTwoNumbers(t *testing.T) {
 	if n.Value != 0 {
 		t.Errorf("0 != %d\n", n.Value)
 	}
+
+	/*
+		ll1 := sll.NewSinglyNode(1)
+		prev := ll1
+		for i := 1; i < 30; i++ {
+			ll := sll.NewSinglyNode(0)
+			prev.Next = ll
+			prev = ll
+		}
+		prev.Next = sll.NewSinglyNode(1)
+
+		ll2 := sll.NewSinglyNode(5)
+		ll2.Next = sll.NewSinglyNode(6)
+		ll2.Next.Next = sll.NewSinglyNode(4)
+	*/
 }
