21a2d3f731e66b6c91efb99824b6ab1ed1d1b9c1
Parth Shah <parth@Parths-Macintosh.local>
2021-11-07 18:13:04 -0800

    Refinements to insertion sort

--- dsandalgo/coursework/sorting/sorting.go
+++ dsandalgo/coursework/sorting/sorting.go
@@ -47,40 +47,22 @@ func SelectionSort(a []int) []int {
 	return a // Worst case O(n^2)
 }
 
-func inplaceInsert(arr []int, ind int, elem int) {
-	if ind >= len(arr) {
-		return
-	}
-
-	prev := arr[ind]
-
-	for i := ind + 1; i < len(arr); i++ { // O(n)
-		temp := arr[i]
-		arr[i] = prev
-		prev = temp
-	}
-
-	arr[ind] = elem
-}
-
 func InsertionSort(a []int) []int {
-	for i := 1; i < len(a); i++ { // O(n)
-		j := i - 1
-		for ; j >= 0; j-- { // O(n)
-			if a[j] < a[i] {
-				inplaceInsert(a[:i+1], j+1, a[i]) // Go slices are passed by value, but the value is a header (which includes a ptr)
-				break
-			}
-		}
-		if j < 0 && a[i] < a[0] {
-			inplaceInsert(a[:i+1], 0, a[i])
+	for i := 1; i < len(a); i++ {
+		// Sort of a reverse bubble sort
+		// 	Also implied, only start the reverse loop if the last elem of the sublist
+		//  has a greater element.
+		for j := i; j > 0 && a[j-1] > a[j]; j-- {
+			temp := a[j-1]
+			a[j-1] = a[j]
+			a[j] = temp
 		}
 	}
 	return a
 }
 
 func main() {
-	fmt.Println(SelectionSort([]int{8, 3, 2, 1, 5, 7, 7, 0}))
-	fmt.Println(SelectionSort([]int{0, 1, 2, 3, 5, 7, 7, 8}))
-	fmt.Println(SelectionSort([]int{7, 9, 100, 11, -5, -7, 0, 10}))
+	fmt.Println(InsertionSort([]int{8, 3, 2, 1, 5, 7, 7, 0}))
+	fmt.Println(InsertionSort([]int{0, 1, 2, 3, 5, 7, 7, 8}))
+	fmt.Println(InsertionSort([]int{7, 9, 100, 11, -5, -7, 0, 10}))
 }
