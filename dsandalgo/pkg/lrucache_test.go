e3b9c161162791f62a00626a92677775c5b33615
parthrs <parthrshah@live.com>
2023-12-05 18:18:08 -0800

    Add LRU cache tests

--- dsandalgo/pkg/lrucache_test.go
+++ dsandalgo/pkg/lrucache_test.go
@@ -53,3 +53,64 @@ func TestLRUCache(t *testing.T) {
 		t.Errorf("Cache tail.Previous value incorrect (expected 100, got %d)", got)
 	}
 }
+
+// Cache with size 1
+func TestLRUCacheLcTest1(t *testing.T) {
+	c := NewLRUCache(1)
+	c.put(2, 1)
+	if result := c.get(2); result != 1 {
+		t.Errorf("c.get(2) - Expected %v, Got %v", 1, result)
+	}
+	c.put(3, 2)
+	if result := c.get(2); result != -1 {
+		t.Errorf("c.get(2) - Expected %v, Got %v", -1, result)
+	}
+	if result := c.get(3); result != 2 {
+		t.Errorf("c.get(3) - Expected %v, Got %v", 2, result)
+	}
+}
+
+// Overwriting an existing key
+func TestLRUCacheLcTest2(t *testing.T) {
+	c := NewLRUCache(2)
+	if result := c.get(2); result != -1 {
+		t.Errorf("c.get(2) - Expected %v, Got %v", -1, result)
+	}
+	c.put(2, 6)
+	if result := c.get(1); result != -1 {
+		t.Errorf("c.get(1) - Expected %v, Got %v", -1, result)
+	}
+	c.put(1, 5)
+	c.put(1, 2)
+	if result := c.get(1); result != 2 {
+		t.Errorf("c.get(1) - Expected %v, Got %v", 2, result)
+	}
+	if result := c.get(2); result != 6 {
+		t.Errorf("c.get(3) - Expected %v, Got %v", 6, result)
+	}
+}
+
+// ["LRUCache","get","get","put","get","put","put","put","put","get","put"]
+// [[1],[6],[8],[12,1],[2],[15,11],[5,2],[1,15],[4,2],[5],[15,15]]
+
+func TestLRUCacheLcTest3(t *testing.T) {
+	c := NewLRUCache(1)
+	if result := c.get(6); result != -1 {
+		t.Errorf("c.get(6) - Expected %v, Got %v", -1, result)
+	}
+	if result := c.get(8); result != -1 {
+		t.Errorf("c.get(8) - Expected %v, Got %v", -1, result)
+	}
+	c.put(12, 1)
+	if result := c.get(2); result != -1 {
+		t.Errorf("c.get(2) - Expected %v, Got %v", -1, result)
+	}
+	c.put(15, 11)
+	c.put(5, 2)
+	c.put(1, 15)
+	c.put(4, 2)
+	if result := c.get(5); result != -1 {
+		t.Errorf("c.get(5) - Expected %v, Got %v", -1, result)
+	}
+	c.put(15, 15)
+}
