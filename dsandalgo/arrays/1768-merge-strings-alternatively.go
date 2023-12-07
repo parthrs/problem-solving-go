256818c30a877e62eea49bb4c354df8d662980d4
parthrs <parthrshah@live.com>
2023-12-06 17:27:00 -0800

    Add 1768 solution only

---
+++ dsandalgo/arrays/1768-merge-strings-alternatively.go
@@ -0,0 +1,15 @@
+package arrays
+
+func MergeAlternately(word1 string, word2 string) (result string) {
+	i := 0
+	for i < len(word1) || i < len(word2) {
+		if i < len(word1) {
+			result += string(word1[i])
+		}
+		if i < len(word2) {
+			result += string(word2[i])
+		}
+		i++
+	}
+	return
+}
