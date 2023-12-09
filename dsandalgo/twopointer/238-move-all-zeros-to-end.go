d14d58d5b2f94cbc99fdc443e81455cebef1663e
parthrs <parthrshah@live.com>
2023-12-09 10:47:37 -0800

    Add some comments

--- dsandalgo/twopointer/238-move-all-zeros-to-end.go
+++ dsandalgo/twopointer/238-move-all-zeros-to-end.go
@@ -10,13 +10,24 @@ Input:  [2,1,0,0,3,2,4,0]
 Output: [2,1,3,2,4,0,0,0]
 */
 
+// MoveZerosToEnd gathers all zeros and moves
+// them forward, collecting them.
+// To maintain order we start both ptrs from LHS.
+// One ptr moves rightward and finds non-zero elems,
+// if found, it swaps with elem pointed by second ptr.
+// Second ptr only moves if swap was done.
+// What happens if the second ptr is pointing to a non-zero elem?
+// A swap happens in place, and it is moved rightward.
+// Ultimately both ptrs keep pace, until the first finds a zero,
+// no swap happens till it finds a non-zero elem and it keeps moving
+// rightward.
 func MoveZerosToEnd(nums []int) []int {
 	index := 0
 	for finder := range nums {
 		if nums[finder] != 0 {
-			if index != finder {
-				nums[index], nums[finder] = nums[finder], nums[index]
-			}
+			//if index != finder { // Avoids unnecessary swap if both point to same elem
+			nums[index], nums[finder] = nums[finder], nums[index]
+			//}
 			index++
 		}
 	}
