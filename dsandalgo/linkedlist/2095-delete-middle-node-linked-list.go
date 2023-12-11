2bd8cba6db92f7755dd7749652c80b0549a0a4d4
parthrs <parthrshah@live.com>
2023-12-10 21:35:10 -0800

    Add solution for lc-2095

---
+++ dsandalgo/linkedlist/2095-delete-middle-node-linked-list.go
@@ -0,0 +1,48 @@
+package linkedlist
+
+import (
+	. "github.com/parthrs/LetsGo/dsandalgo/pkg"
+)
+
+/*
+https://en.wikipedia.org/wiki/Cycle_detection#Tortoise_and_hare
+The crux of this method uses Robert Floyd's (which as per wikipedia
+should not be attributed to him) slow and fast pointer approach.
+Same method to find if the linked list is looped.
+
+The fast move 2x faster than slow, hence when fast is done,
+slow is smack in the middle of the list.
+
+Since we need to return head, we create a backup called
+dummyHead.
+
+Prev is needed because in singly list we don't have a prev
+pointer, and we can't remove the middle node (slow) if we
+can't change the prev node's pointer.
+
+There are some smart approaches that:
+- start slow, fast and prev to the new node
+  before head, and,
+- check for fast.Next && fast.Next.Next (crucial for this
+  approach to work)
+this way, slow points to one node behind of middle.
+*/
+
+func DeleteMiddle(head *SinglyNode) *SinglyNode {
+	if head.Next == nil {
+		return nil
+	}
+	prev := &SinglyNode{0, head}
+	dummyHead, slow, fast := head, head, head
+	for fast != nil && fast.Next != nil {
+		prev = prev.Next
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	// truly remove the node
+	// by removing it referencing the next
+	// node
+	prev.Next = slow.Next
+	slow.Next = nil
+	return dummyHead
+}
