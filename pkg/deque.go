dc09addcb7562eb6b27549f44cf2480d2066c976
parthrs <parthrshah@live.com>
2023-12-13 18:59:01 -0800

    Fix deque spelling

--- pkg/deque.go
+++ pkg/deque.go
@@ -1,34 +1,46 @@
 package pkg
 
-type Dequeue struct {
+/*
+Here are the ways a deque could be implemented:
+ - Using a dynamic array:
+   - Insertions to the front are ammortized to O(1)
+	 - Insertions/deletions in the middle are O(n)
+	 - Random access by index is O(1)
+ - Using a LinkedList:
+   - All insertions are O(1)
+	 - Insertions/deletions in the middle are O(1)
+	 - Random access by index is O(n)
+*/
+
+type Deque struct {
 	length   int32
 	li       []int32
 	capacity int32
 }
 
-func NewDequeue(size int32) Dequeue {
-	return Dequeue{
+func NewDeque(size int32) Deque {
+	return Deque{
 		length:   0,
 		li:       []int32{},
 		capacity: size,
 	}
 }
 
-func (d *Dequeue) appendLeft(k int32) {
+func (d *Deque) appendLeft(k int32) {
 	if d.length < d.capacity {
 		d.li = append([]int32{k}, d.li...)
 		d.length += 1
 	}
 }
 
-func (d *Dequeue) appendRight(k int32) {
+func (d *Deque) appendRight(k int32) {
 	if d.length < d.capacity {
 		d.li = append(d.li, []int32{k}...)
 		d.length += 1
 	}
 }
 
-func (d *Dequeue) popLeft() int32 {
+func (d *Deque) popLeft() int32 {
 	var retVal int32 = -1
 	if d.length > 0 {
 		retVal = d.li[0]
@@ -39,7 +51,7 @@ func (d *Dequeue) popLeft() int32 {
 	return retVal
 }
 
-func (d *Dequeue) popRight() int32 {
+func (d *Deque) popRight() int32 {
 	var retVal int32 = -1
 	if d.length > 0 {
 		retVal = d.li[d.length-1] // note the use of length - 1 since d.length gives the len, but really 'points' to the next location
--- pkg/deque_test.go
+++ pkg/deque_test.go
@@ -2,21 +2,21 @@ package pkg
 
 import "testing"
 
-func TestDequeue(t *testing.T) {
-	dq := NewDequeue(5)
+func TestDeque(t *testing.T) {
+	dq := NewDeque(5)
 	if dq.length != int32(0) {
-		t.Error("Length of empty dequeue not zero")
+		t.Error("Length of empty Deque not zero")
 	}
 	if dq.capacity != int32(5) {
-		t.Error("Capacity of dequeue not as declared")
+		t.Error("Capacity of Deque not as declared")
 	}
 	dq.appendLeft(1)
 	if dq.length != int32(1) {
-		t.Error("Length of dequeue not as expected")
+		t.Error("Length of Deque not as expected")
 	}
 	dq.appendRight(2)
 	if dq.length != int32(2) {
-		t.Error("Length of dequeue not as expected")
+		t.Error("Length of Deque not as expected")
 	}
 	if dq.popLeft() != int32(1) {
 		t.Error("Popleft didn't return expected item")
