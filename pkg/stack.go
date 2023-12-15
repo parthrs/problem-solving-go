aa61d08f7f0ee3159e165bb0645bc2dced395eb8
parthrs <parthrshah@live.com>
2023-12-14 18:26:37 -0800

    Stack operation in O(1)

--- pkg/stack.go
+++ pkg/stack.go
@@ -1,19 +1,53 @@
 package pkg
 
+/*
+Implementing a stack that does not involve memory
+re-allocation (via an underlying array) for any
+push and pop operations.
+
+Implementing using a singly linked list
+
+The difference between this and a standalone
+singly linked list implementation: tracking
+the size.
+
+Push and pop are O(1)
+*/
+
 type Stack[T any] struct {
-	S []T
+	Head *SinglyNode[T]
+	Len  int
+	Cap  int
 }
 
-func NewStack[T any]() *Stack[T] {
-	return &Stack[T]{S: []T{}}
+func NewStack[T any](capacity int) *Stack[T] {
+	return &Stack[T]{
+		Len:  0,
+		Cap:  capacity,
+		Head: nil,
+	}
 }
 
-func (s *Stack[T]) Add(elem T) {
-	s.S = append(s.S, elem)
+func (s *Stack[T]) Push(elem T) bool {
+	if s.Len == s.Cap {
+		return false // Element not added
+	}
+	node := NewSinglyNode[T](elem)
+	node.Next = s.Head
+	s.Head = node
+	s.Len++
+	return true
 }
 
-func (s *Stack[T]) Pop() T {
-	elem := s.S[len(s.S)-1]
-	s.S = s.S[:len(s.S)-1]
-	return elem
+func (s *Stack[T]) Pop() (retVal T) {
+	if s.Head == nil {
+		return // return the default value for T
+	}
+	n := s.Head
+	s.Head = s.Head.Next
+	// Remove any reference this node is making to other nodes
+	n.Next = nil
+	retVal = n.Value
+	s.Len--
+	return
 }
--- pkg/stack_test.go
+++ pkg/stack_test.go
@@ -2,14 +2,47 @@ package pkg
 
 import "testing"
 
-func TestStack(t *testing.T) {
-	s := NewStack[int]()
-	s.Add(5)
-	s.Add(10)
-	if len(s.S) != 2 {
-		t.Errorf("Len expected to be 2, got %v", len(s.S))
+func TestNewStack(t *testing.T) {
+	// Stack with capacity of 5 elems
+	s := NewStack[int](5)
+	// Check len and capacity
+	if length := s.Len; length != 0 {
+		t.Errorf("Len expected to be 0, got %v", length)
 	}
-	if pop := s.Pop(); pop != 10 {
-		t.Errorf("Expected elem 10, got %v", pop)
+	if capacity := s.Cap; capacity != 5 {
+		t.Errorf("Capacity expected to be 5, got %v", capacity)
+	}
+
+	// Push 5 elems 4 through 0 in order
+	// Check for return status (of push)
+	for i := 4; i >= 0; i++ {
+		status := s.Push(i)
+		if !status {
+			t.Errorf("Expected true while push(%d), got %v", i, status)
+		}
+	}
+
+	// Push beyond capacity, expect unsuccessful
+	// operation
+	status := s.Push(5)
+	if status {
+		t.Errorf("Expected false while push(5), got %v", status)
+	}
+
+	// Expect length to be intact and full
+	if length := s.Len; length != 5 {
+		t.Errorf("Len expected to be 5, got %v", length)
+	}
+
+	// Pop two elems and check return values
+	// Check length after two pop operations
+	if val := s.Pop(); val != 0 {
+		t.Errorf("Expected val to be 0, got %d", val)
+	}
+	if val := s.Pop(); val != 1 {
+		t.Errorf("Expected val to be 1, got %d", val)
+	}
+	if length := s.Len; length != 3 {
+		t.Errorf("Len expected to be 3, got %v", length)
 	}
 }
