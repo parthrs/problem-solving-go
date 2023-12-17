ae1b30e2d13d6e780d4bcc5df0cc8334a9e9d171
parthrs <parthrshah@live.com>
2023-12-17 13:47:34 -0800

    Fix package import for lc-876

--- leet-code/linkedlist/876-find-linked-list-middle-node.go
+++ leet-code/linkedlist/876-find-linked-list-middle-node.go
@@ -1,18 +1,17 @@
 package linkedlist
 
 import (
-	. "github.com/parthrs/problem-solving-go/pkg"
+	"github.com/parthrs/problem-solving-go/pkg"
 )
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+/*
+To-Do: What if we wish to know the last and the middle node?
+Currently the last node points to nil.
 
-func MiddleNode(head *SinglyNode[int]) *SinglyNode[int] {
+Also To-Do: What are the various outcomes of starting slow and
+fast at different positions - like slow at -1 and fast at 0.
+*/
+func MiddleNode(head *pkg.SinglyNode[int]) *pkg.SinglyNode[int] {
 	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
 		slow, fast = slow.Next, fast.Next.Next
