88a1d032b2c6df9111c7da23497b8f25d20b396a
parthrs <parthrshah@live.com>
2023-12-19 12:31:41 -0800

    Optimize solution for lc-1004

--- leet-code/slidingwindow/1004-max-consecutive-ones.go
+++ leet-code/slidingwindow/1004-max-consecutive-ones.go
@@ -22,3 +22,32 @@ func LlongestOnes(nums []int, k int) int {
 	}
 	return maxCount
 }
+
+// The window size, i.e. the distance between the
+// left and the right pointers, is the max consecutive
+// ones with flipping max k 0's
+func LongestOnes(nums []int, k int) int {
+	left := 0
+
+	// Right pointer for the window
+	// is greedily moved right every iteration
+	// left is only moved up if we've exhausted
+	// k (i.e. the ability to flip 0's to 1's)
+	for _, elem := range nums {
+		if elem == 0 {
+			k--
+		}
+		// Each time k is negative, move left up
+		// i.e. push the window forward and not grow
+		// it.
+		// If while moving the window, the number leaving
+		// is 0, increase k.
+		if k < 0 {
+			if nums[left] == 0 {
+				k++
+			}
+			left++
+		}
+	}
+	return len(nums) - left
+}
