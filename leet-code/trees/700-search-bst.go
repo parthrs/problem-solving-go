bf1be95d4f231ccc046499a80b447022c282299a
parthrs <parthrshah@live.com>
2024-01-15 18:28:37 -0800

    Add solution to lc-700

---
+++ leet-code/trees/700-search-bst.go
@@ -0,0 +1,32 @@
+package trees
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func SearchBST(root *TreeNode, val int) *TreeNode {
+	// edge case
+	if root == nil {
+		return nil
+	}
+
+	// base case
+	if root.Val == val {
+		return root
+	}
+
+	// Recursive case(s)
+	// We don't need to check if the left
+	// or right subtrees are nil here, they
+	// will be the first thing caught (in the edge
+	// case section) on the recursive call
+	if val < root.Val {
+		return SearchBST(root.Left, val)
+	} else if val > root.Val {
+		return SearchBST(root.Right, val)
+	} else {
+		return nil
+	}
+}
